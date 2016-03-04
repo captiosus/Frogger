e8dcd42c7cb78c7b8e42cd5684ab3bb8be8cefb8
bleepbloopsify <bleepbloopsify@gmail.com>
2016-03-04 12:31:54 -0500

    pls

--- frogger.js
+++ frogger.js
@@ -1,2 +1,15 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var requestId;
+
+
+var setupFrog = function(){
+  var x = 210;
+  var y = 600;
+  var width = 60;
+
+  var drawFrog = function(){
+
+    requestId = window.requestAnimationFrame(drawFrog);
+  };
+};
